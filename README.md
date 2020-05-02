b4a847eb8e93e8f6134dad1749a7306ecfca69fc
Jaume Delclòs Coll <cosarara@cosarara.me>
2020-05-02 21:24:58 +0200

    add copyright and licensing info

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ App to easily upload images from android to HTTP servers using the share functio
 <img src="screenshots/collage.png" width="400">
 
 Watch the [video](screenshots/vid1.mp4).
+
+# Licensing
+The code in this repository is covered under the GPLv3 or later.
+See the LICENSE file for the license text.
--- app/src/main/java/me/cosarara/up/SettingsActivity.java
+++ app/src/main/java/me/cosarara/up/SettingsActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 Jaume Delclòs Coll <up@cosarara.me>
+ *
+ * This file is part of Up.
+ *
+ * Up is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Up is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Up.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.cosarara.up;
 
 import android.content.SharedPreferences;
